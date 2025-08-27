dad49afe08e9e125c82f135813e568477f7d4e11
Egor Pisarev <egor.pisarev@gmail.com>
2025-08-28 00:32:23 +0700

    cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['http://localhost:5173'], // фронт на vite
+    origin: ['http://localhost:5173', 'https://game.almet22.ru'], // фронт на vite
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
